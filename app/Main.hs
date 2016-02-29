21b64a9fd1c59e6723ea32ecb01b782d1108149a
Arpad Kovacs <minhmeoke@gmail.com>
2016-02-28 22:29:07 -0800

    Add ability to spawn remote processes

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
 import Control.Distributed.Process
+import Control.Distributed.Process.Closure
 import Control.Distributed.Process.Node
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
+
+sampleTask :: (Int, String) -> Process ()
+sampleTask (t, s) = liftIO (threadDelay (t * 1000000)) >> say s
+
+remotable ['sampleTask]
+
+myRemoteTable :: RemoteTable
+myRemoteTable = Main.__remoteTable initRemoteTable
+
 
 replyBack :: (ProcessId, String) -> Process ()
 replyBack (sender, msg) = send sender msg
@@ -12,9 +24,16 @@ logMessage msg = say $ "handling " ++ msg
 
 main :: IO ()
 main = do
-  Right t <- createTransport "127.0.0.1" "10501" defaultTCPParameters
-  node <- newLocalNode t initRemoteTable
+  Right transport <- createTransport "127.0.0.1" "10501" defaultTCPParameters
+  node <- newLocalNode transport myRemoteTable
   runProcess node $ do
+    -- Test spawning process
+    us <- getSelfNode
+    _ <- spawnLocal $ sampleTask (1 :: Int, "using spawnLocal")
+    pid <- spawn us $ $(mkClosure 'sampleTask) (1 :: Int, "using spawn")
+    liftIO $ threadDelay 2000000
+
+
     -- Spawn another worker on the local node
     echoPid <- spawnLocal $ forever $ do
       -- Test our matches in order against each message in the queue
@@ -30,7 +49,7 @@ main = do
     send echoPid (self, "hello")
 
     -- `expectTimeout` waits for a message or times out after "delay"
-    m <- expectTimeout 1000000
+    m <- expectTimeout 2000000
     case m of
       -- Die immediately - throws a ProcessExitException with the given reason.
       Nothing  -> die "nothing came back!"
