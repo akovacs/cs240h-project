351693f9eb05bdd2271a887abb7c4be4e34f6217
Arpad Kovacs <minhmeoke@gmail.com>
2016-02-28 23:13:44 -0800

    Master-slave example

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+import System.Environment (getArgs)
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
 import Control.Distributed.Process
 import Control.Distributed.Process.Closure
 import Control.Distributed.Process.Node
+import Control.Distributed.Process.Backend.SimpleLocalnet
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
 
 sampleTask :: (Int, String) -> Process ()
@@ -24,33 +26,20 @@ logMessage msg = say $ "handling " ++ msg
 
 main :: IO ()
 main = do
-  Right transport <- createTransport "127.0.0.1" "10501" defaultTCPParameters
-  node <- newLocalNode transport myRemoteTable
-  runProcess node $ do
-    -- Test spawning process
-    us <- getSelfNode
-    _ <- spawnLocal $ sampleTask (1 :: Int, "using spawnLocal")
-    pid <- spawn us $ $(mkClosure 'sampleTask) (1 :: Int, "using spawn")
-    liftIO $ threadDelay 2000000
+  args <- getArgs
+
+  case args of
+    ["master", host, port] -> do
+      backend <- initializeBackend host port initRemoteTable
+      startMaster backend (master backend)
+    ["slave", host, port] -> do
+      backend <- initializeBackend host port initRemoteTable
+      startSlave backend
 
 
-    -- Spawn another worker on the local node
-    echoPid <- spawnLocal $ forever $ do
-      -- Test our matches in order against each message in the queue
-      receiveWait [match logMessage, match replyBack]
-
-    -- The `say` function sends a message to a process registered as "logger".
-    -- By default, this process simply loops through its mailbox and sends
-    -- any received log message strings it finds to stderr.
-
-    say "send some messages!"
-    send echoPid "hello"
-    self <- getSelfPid
-    send echoPid (self, "hello")
-
-    -- `expectTimeout` waits for a message or times out after "delay"
-    m <- expectTimeout 2000000
-    case m of
-      -- Die immediately - throws a ProcessExitException with the given reason.
-      Nothing  -> die "nothing came back!"
-      Just s -> say $ "got " ++ s ++ " back!"
+master :: Backend -> [NodeId] -> Process ()
+master backend slaves = do
+  -- Do something interesting with the slaves
+  liftIO . putStrLn $ "Slaves: " ++ show slaves
+  -- Terminate the slaves when the master terminates (this is optional)
+  terminateAllSlaves backend
---
+++ app/RemoteProcess.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
+import Control.Distributed.Process
+import Control.Distributed.Process.Closure
+import Control.Distributed.Process.Node
+import Network.Transport.TCP (createTransport, defaultTCPParameters)
+
+sampleTask :: (Int, String) -> Process ()
+sampleTask (t, s) = liftIO (threadDelay (t * 1000000)) >> say s
+
+remotable ['sampleTask]
+
+myRemoteTable :: RemoteTable
+myRemoteTable = Main.__remoteTable initRemoteTable
+
+
+replyBack :: (ProcessId, String) -> Process ()
+replyBack (sender, msg) = send sender msg
+
+logMessage :: String -> Process ()
+logMessage msg = say $ "handling " ++ msg
+
+main :: IO ()
+main = do
+  Right transport <- createTransport "127.0.0.1" "10501" defaultTCPParameters
+  node <- newLocalNode transport myRemoteTable
+  runProcess node $ do
+    -- Test spawning process
+    us <- getSelfNode
+    _ <- spawnLocal $ sampleTask (1 :: Int, "using spawnLocal")
+    pid <- spawn us $ $(mkClosure 'sampleTask) (1 :: Int, "using spawn")
+    liftIO $ threadDelay 2000000
+
+
+    -- Spawn another worker on the local node
+    echoPid <- spawnLocal $ forever $ do
+      -- Test our matches in order against each message in the queue
+      receiveWait [match logMessage, match replyBack]
+
+    -- The `say` function sends a message to a process registered as "logger".
+    -- By default, this process simply loops through its mailbox and sends
+    -- any received log message strings it finds to stderr.
+
+    say "send some messages!"
+    send echoPid "hello"
+    self <- getSelfPid
+    send echoPid (self, "hello")
+
+    -- `expectTimeout` waits for a message or times out after "delay"
+    m <- expectTimeout 2000000
+    case m of
+      -- Die immediately - throws a ProcessExitException with the given reason.
+      Nothing  -> die "nothing came back!"
+      Just s -> say $ "got " ++ s ++ " back!"
