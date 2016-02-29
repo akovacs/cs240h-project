61915e8c7db6f9942070cf5af55901096ab1eb8c
Arpad Kovacs <minhmeoke@gmail.com>
2016-02-29 00:18:40 -0800

    Executing process on slave

--- app/Main.hs
+++ app/Main.hs
@@ -9,20 +9,24 @@ import Control.Distributed.Process.Node
 import Control.Distributed.Process.Backend.SimpleLocalnet
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
 
+
 sampleTask :: (Int, String) -> Process ()
-sampleTask (t, s) = liftIO (threadDelay (t * 1000000)) >> say s
-
-remotable ['sampleTask]
-
-myRemoteTable :: RemoteTable
-myRemoteTable = Main.__remoteTable initRemoteTable
-
+sampleTask (secondsDelay, outputString) = liftIO (threadDelay (secondsDelay * 1000000)) >> say outputString
 
 replyBack :: (ProcessId, String) -> Process ()
 replyBack (sender, msg) = send sender msg
 
 logMessage :: String -> Process ()
-logMessage msg = say $ "handling " ++ msg
+logMessage msg = do 
+  liftIO $ putStrLn msg
+  say $ "handling " ++ msg
+
+
+remotable ['sampleTask, 'logMessage]
+
+myRemoteTable :: RemoteTable
+myRemoteTable = Main.__remoteTable initRemoteTable
+
 
 main :: IO ()
 main = do
@@ -39,7 +43,25 @@ main = do
 
 master :: Backend -> [NodeId] -> Process ()
 master backend slaves = do
-  -- Do something interesting with the slaves
+  -- Print list of slaves
   liftIO . putStrLn $ "Slaves: " ++ show slaves
+  -- Start heartbeat process on all slaves
+  heartbeatPids <- mapM startHeartbeatListener slaves
+  _ <- mapM sendHeartbeat heartbeatPids
   -- Terminate the slaves when the master terminates (this is optional)
+  liftIO $ threadDelay 2000000
   terminateAllSlaves backend
+
+-- Heartbeat process just echoes message
+startHeartbeatListener :: NodeId -> Process ProcessId
+startHeartbeatListener nodeId = spawnLocal $ logMessage "using spawnLocal"
+--startHeartbeatListener nodeId = spawn nodeId $ $(mkClosure 'logMessage) ("using spawn")
+
+-- Send heartbeat
+sendHeartbeat :: ProcessId -> Process()
+sendHeartbeat receiverProcessId = send receiverProcessId "hello"
+
+-- Initially Slave just echoes heartbeat message
+slave :: Backend -> Process()
+slave backend = forever $ do
+  receiveWait [match logMessage, match replyBack]
